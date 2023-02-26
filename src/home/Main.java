15db657624c3f4b74c7f65b61a53d0cd210fd9c3
oktastar <oktastar@seznam.cz>
2023-02-26 16:36:33 +0200

    Empty constructor class Smartphone, method start_sm in Smartphone;  1 empty  instances, 3 in, 1 out,  1 out start_sm in Main.

--- src/home/Main.java
+++ src/home/Main.java
@@ -2,13 +2,13 @@ package home;
 
 public class Main {
     public static void main(String[] args) {
-        Notebook andrei = new Notebook("Acer", 1.0F, 512);
-        Notebook galina = new Notebook("Asus", 3.0F, 256);
+        Smartphone andrei = new Smartphone();
 
-        System.out.println(andrei.ssd);
-        System.out.println(galina.brand_nb);
-        andrei.start_this_nb();
-        galina.start_get_nb();
+        andrei.brand_sm = "HTC";
+        andrei.RAM = 8;
+        andrei.memory = 128;
+        System.out.println(andrei.brand_sm);
+        andrei.start_sm();
         }
 
 }  // public class Main
---
+++ src/home/Smartphone.java
@@ -0,0 +1,14 @@
+package home;
+
+public class Smartphone {
+    String brand_sm;
+    int RAM;
+    int memory;
+
+    public Smartphone() {
+    }
+    void start_sm(){
+        System.out.println("Запускается "+brand_sm+" c RAM "+RAM+" Gb, с памятью "+memory+" Gb.");
+    }
+
+}  // public class Smartphone
